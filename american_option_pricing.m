da917f7046df1ed02e88c779473e6d254ae09725
anishLearnsToCode <anish_@outlook.com>
2020-11-30 17:32:40 +0530

    renames files

---
+++ american_option_pricing.m
@@ -0,0 +1,295 @@
+% Mathematical Modelling and Simulation Project
+% Pricing American Basket Options 
+% Using Multivariate Geomrtric Brownian Motion (GBM) Process 
+% Anish Sachdeva 
+% DTU/2K16/MC/013
+% Delhi Technological University
+% Mathemtical Modelling and Simulation (MC-409)
+
+clc;
+clear;
+close all;
+
+% Import the Supporting Historical Dataset
+% Load a daily historical dataset of 3-month Euribor, the trading dates 
+% spanning the interval 07-Feb-2001 to 24-Apr-2006, and the closing index 
+% levels of the following representative large-cap equity indices:
+% 1. TSX Composite (Canada)
+% 2. CAC 40 (France)
+% 3. DAX (Germany)
+% 4. Nikkei 225 (Japan)
+% 5. FTSE 100 (UK)
+% 6. S&P 500 (US)
+
+load Data_GlobalIdx2;
+dates = datetime(dates, 'ConvertFrom', 'datenum');
+
+% The following plots illustrate this data. Specifically, the plots show 
+% the relative price movements of each index and the Euribor risk-free rate proxy. 
+% The initial level of each index has been normalized to unity to 
+% facilitate the comparison of relative performance over the historical record
+
+nIndices  = size(Data,2)-1;     % # of indices
+
+prices = Data(:,1:end-1);
+
+yields = Data(:,end);             % daily effective yields
+yields = 360 * log(1 + yields);   % continuously-compounded, annualized yield
+
+figure;
+plot(dates, ret2tick(tick2ret(prices,'Method','continuous'),'Method','continuous'))
+xlabel('Date')
+ylabel('Index Value')
+title ('Normalized Daily Index Closings')
+legend(series{1:end-1}, 'Location', 'NorthWest')
+waitforbuttonpress;
+
+% Plotting the Risk Free Rate (3-Month Euribor Continuously-Compounded)
+
+figure;
+plot(dates, 100 * yields)
+xlabel('Date')
+ylabel('Annualized Yield (%)')
+title('Risk Free Rate (3-Month Euribor Continuously-Compounded)')
+waitforbuttonpress;
+
+% Fitting a Pareto Tail Curve In for every country's index and plotting
+% for a particular country (France, DAX)
+
+returns = tick2ret(prices,'Method','continuous');        % convert prices to returns
+returns = returns - mean(returns);  % center the returns
+index   = 2;                                       % France stored in column 2
+
+figure;
+plot(dates(2:end), returns(:,index))
+xlabel('Date')
+ylabel('Return')
+title(['Daily Logarithmic Centered Returns: ' series{index}])
+waitforbuttonpress;
+
+% Creating the Pereto tails with smoothened Gaussian Kernel and EVT (Extreme
+% Value Theory applied to 10% of each tail)
+
+tailFraction = 0.1;               % decimal fraction allocated to each tail
+tails = cell(nIndices,1);  % cell array of Pareto tail objects
+
+for i = 1:nIndices
+    tails{i} = paretotails(returns(:,i), tailFraction, 1 - tailFraction, 'kernel');
+end
+
+% Plotting a Semi-Parametric/Peicewise distribution as an Example
+
+minProbability = cdf(tails{index}, (min(returns(:,index))));
+maxProbability = cdf(tails{index}, (max(returns(:,index))));
+
+pLowerTail = linspace(minProbability  , tailFraction    , 200); % lower tail
+pUpperTail = linspace(1 - tailFraction, maxProbability  , 200); % upper tail
+pInterior  = linspace(tailFraction    , 1 - tailFraction, 200); % interior
+
+figure;
+plot(icdf(tails{index}, pLowerTail), pLowerTail, 'red'  , 'LineWidth', 2)
+hold on
+grid on
+plot(icdf(tails{index}, pInterior) , pInterior , 'black', 'LineWidth', 2)
+plot(icdf(tails{index}, pUpperTail), pUpperTail, 'blue' , 'LineWidth', 2)
+
+limits = axis;
+x = linspace(limits(1), limits(2));
+plot(x, normcdf(x, 0, std(returns(:,index))), 'green', 'LineWidth', 2)
+
+fig = gcf;
+fig.Color = [1 1 1];
+hold off
+xlabel('Centered Return')
+ylabel('Probability')
+title (['Semi-Parametric/Piecewise CDF: ' series{index}])
+legend({'Pareto Lower Tail' 'Kernel Smoothed Interior' ...
+        'Pareto Upper Tail' 'Gaussian with Same \sigma'}, 'Location', 'NorthWest')
+waitforbuttonpress;
+
+% Copula Calibration
+% When the uniform variates are transformed by the empirical CDF of each 
+% margin, the calibration method is often known as canonical maximum 
+% likelihood (CML). The following code segment first transforms the daily 
+% centered returns to uniform variates by the piecewise, semi-parametric 
+% CDFs derived above. It then fits the Gaussian and t copulas to the transformed data:
+
+U = zeros(size(returns));
+
+for i = 1:nIndices
+    U(:,i) = cdf(tails{i}, returns(:,i));    % transform each margin to uniform
+end
+
+options     = statset('Display', 'off', 'TolX', 1e-4);
+[rhoT, DoF] = copulafit('t', U, 'Method', 'ApproximateML', 'Options', options);
+rhoG        = copulafit('Gaussian', U);
+
+ % linear correlation matrix of daily returns
+ disp('linear correlation matrix of daily returns');
+ disp(corrcoef(returns));
+ waitforbuttonpress;
+ 
+ % linear correlation matrix of the optimized Gaussian copula
+ disp('linear correlation matrix of the optimized Gaussian copula');
+ disp(rhoG);
+ waitforbuttonpress;
+ 
+ % linear correlation matrix of the optimized t copula
+ disp('linear correlation matrix of the optimized t copula');
+ disp(rhoT);
+ waitforbuttonpress;
+ 
+ % scalar degrees of freedom parameter of the optimized t copula
+ disp('scalar degrees of freedom parameter of the optimized t copula');
+ disp(DoF);
+ waitforbuttonpress;
+ 
+% Copula Simulation
+% Now that the copula parameters have been estimated, we simulate 
+% jointly-dependent uniform variates using the function copularnd.
+ 
+figure;
+nPoints = 10000;                          % # of simulated observations
+
+s = RandStream.getGlobalStream();
+reset(s)
+
+R = zeros(nPoints, nIndices);             % pre-allocate simulated returns array
+U = copularnd('t', rhoT, DoF, nPoints);   % simulate U(0,1) from t copula
+
+for j = 1:nIndices
+    R(:,j) = icdf(tails{j}, U(:,j));
+end
+
+h = scatterhist(R(:,2), R(:,3),'Color','r','Marker','.','MarkerSize',1);
+fig = gcf;
+fig.Color = [1 1 1];
+y1 = ylim(h(1));
+y3 = ylim(h(3));
+xlim(h(1), [-.1 .1])
+ylim(h(1), [-.1 .1])
+xlim(h(2), [-.1 .1])
+ylim(h(3), [(y3(1) + (-0.1 - y1(1)))  (y3(2) + (0.1 - y1(2)))])
+xlabel('France')
+ylabel('Germany')
+title(['t Copula (\nu = ' num2str(DoF,2) ')'])
+waitforbuttonpress;
+
+% Gaussian Copula
+% Now simulate and plot centered returns using the Gaussian copula.
+
+figure;
+reset(s)
+R = zeros(nPoints, nIndices);             % pre-allocate simulated returns array
+U = copularnd('Gaussian', rhoG, nPoints); % simulate U(0,1) from Gaussian copula
+
+for j = 1:nIndices
+    R(:,j) = icdf(tails{j}, U(:,j));
+end
+
+h = scatterhist(R(:,2), R(:,3),'Color','r','Marker','.','MarkerSize',1);
+fig = gcf;
+fig.Color = [1 1 1];
+y1 = ylim(h(1));
+y3 = ylim(h(3));
+xlim(h(1), [-.1 .1])
+ylim(h(1), [-.1 .1])
+xlim(h(2), [-.1 .1])
+ylim(h(3), [(y3(1) + (-0.1 - y1(1)))  (y3(2) + (0.1 - y1(2)))])
+xlabel('France')
+ylabel('Germany')
+title('Gaussian Copula')
+waitforbuttonpress;
+
+% Gaussian Distribution
+% We now simulate and plot centered returns using the Gaussian 
+% distribution, which underlies the traditional Brownian motion model.
+
+figure;
+reset(s)
+R = mvnrnd(zeros(1,nIndices), cov(returns), nPoints);
+h = scatterhist(R(:,2), R(:,3),'Color','r','Marker','.','MarkerSize',1);
+fig = gcf;
+fig.Color = [1 1 1];
+y1 = ylim(h(1));
+y3 = ylim(h(3));
+xlim(h(1), [-.1 .1])
+ylim(h(1), [-.1 .1])
+xlim(h(2), [-.1 .1])
+ylim(h(3), [(y3(1) + (-0.1 - y1(1)))  (y3(2) + (0.1 - y1(2)))])
+xlabel('France')
+ylabel('Germany')
+title('Gaussian Distribution')
+
+% American Option Pricing Using Markov Chain Monte Carlo Simulation
+
+dt       = 1 / 252;                  % time increment = 1 day = 1/252 years
+yields   = Data(:,end);              % daily effective yields
+yields   = 360 * log(1 + yields);    % continuously-compounded, annualized yields
+r        = mean(yields);             % historical 3M Euribor average
+X        = repmat(100, nIndices, 1); % initial state vector
+strike   = sum(X);                   % initialize an at-the-money basket
+
+nTrials  = 100;                      % # of independent trials
+nPeriods = 63;   % # of simulation periods: 63/252 = 0.25 years = 3 months
+
+% sub 
+
+sigma       = std(returns) * sqrt(252);    % annualized volatility
+correlation = corrcoef(returns);           % correlated Gaussian disturbances
+GBM1        = gbm(diag(r(ones(1,nIndices))), diag(sigma), 'StartState', X, ...
+                 'Correlation'             , correlation);
+             
+% create second brownian copula with identity matrix
+
+GBM2 = gbm(diag(r(ones(1    ,nIndices))), eye(nIndices), 'StartState', X);
+
+% sub 
+
+f = LongstaffSchwartz(nPeriods, nTrials);
+disp(f)
+% sub 
+
+reset(s)
+
+simByEuler(GBM1, nPeriods, 'nTrials'  , nTrials, 'DeltaTime', dt, ...
+                          'Processes', f.LongstaffSchwartz);
+
+BrownianMotionCallPrice = f.CallPrice(strike, r);
+BrownianMotionPutPrice  = f.PutPrice (strike, r);
+
+reset(s)
+
+z = Example_CopulaRNG(returns * sqrt(252), nPeriods, 'Gaussian');
+f = LongstaffSchwartz(nPeriods, nTrials);
+
+simByEuler(GBM2, nPeriods, 'nTrials'  , nTrials, 'DeltaTime', dt, ...
+                          'Processes', f.LongstaffSchwartz, 'Z', z);
+
+GaussianCopulaCallPrice = f.CallPrice(strike, r);
+GaussianCopulaPutPrice  = f.PutPrice (strike, r);
+
+% sub 
+
+reset(s)
+
+z = Example_CopulaRNG(returns * sqrt(252), nPeriods, 't');
+f = LongstaffSchwartz(nPeriods, nTrials);
+
+simByEuler(GBM2, nPeriods, 'nTrials'  , nTrials, 'DeltaTime', dt, ...
+                          'Processes', f.LongstaffSchwartz, 'Z', z);
+
+tCopulaCallPrice = f.CallPrice(strike, r);
+tCopulaPutPrice  = f.PutPrice (strike, r);
+
+% sub 
+
+disp(' ')
+fprintf('                    # of Monte Carlo Trials: %8d\n'    , nTrials)
+fprintf('        `            # of Time Periods/Trial: %8d\n\n'  , nPeriods)
+fprintf(' Brownian Motion American Call Basket Price: %8.4f\n'  , BrownianMotionCallPrice)
+fprintf(' Brownian Motion American Put  Basket Price: %8.4f\n\n', BrownianMotionPutPrice)
+fprintf(' Gaussian Copula American Call Basket Price: %8.4f\n'  , GaussianCopulaCallPrice)
+fprintf(' Gaussian Copula American Put  Basket Price: %8.4f\n\n', GaussianCopulaPutPrice)
+fprintf('        t Copula American Call Basket Price: %8.4f\n'  , tCopulaCallPrice)
+fprintf('        t Copula American Put  Basket Price: %8.4f\n'  , tCopulaPutPrice)
--- simulate_option_prices.m
+++ simulate_option_prices.m
@@ -22,7 +22,7 @@ close all;
 % 6. S&P 500 (US)
 
 load Data_GlobalIdx2;
-dates = datetime(dates,'ConvertFrom', 'datenum');
+dates = datetime(dates, 'ConvertFrom', 'datenum');
 
 % sub
 
@@ -31,35 +31,11 @@ nIndices  = size(Data,2)-1;     % # of indices
 prices = Data(:,1:end-1);
 
 yields = Data(:,end);             % daily effective yields
-yields = 360 * log(1 + yields);   % continuously-compounded, annualized yield
-
-figure;
-plot(dates, ret2tick(tick2ret(prices,'Method','continuous'),'Method','continuous'))
-
-xlabel('Date')
-ylabel('Index Value')
-title ('Normalized Daily Index Closings')
-legend(series{1:end-1}, 'Location', 'NorthWest')
-
-% sub
-
-figure;
-plot(dates, 100 * yields)
-xlabel('Date')
-ylabel('Annualized Yield (%)')
-title('Risk Free Rate (3-Month Euribor Continuously-Compounded)')
-
-% sub 
+yields = 360 * log(1 + yields);   % continuously-compounded, annualized yield 
 
 returns = tick2ret(prices,'Method','continuous');        % convert prices to returns
 returns = returns - mean(returns);  % center the returns
 index   = 2;                                       % France stored in column 2
-
-figure;
-plot(dates(2:end), returns(:,index))
-xlabel('Date')
-ylabel('Return')
-title(['Daily Logarithmic Centered Returns: ' series{index}])
 
 % sub 
 
@@ -79,27 +55,10 @@ pLowerTail = linspace(minProbability  , tailFraction    , 200); % lower tail
 pUpperTail = linspace(1 - tailFraction, maxProbability  , 200); % upper tail
 pInterior  = linspace(tailFraction    , 1 - tailFraction, 200); % interior
 
-figure;
-plot(icdf(tails{index}, pLowerTail), pLowerTail, 'red'  , 'LineWidth', 2)
-hold on
-grid on
-plot(icdf(tails{index}, pInterior) , pInterior , 'black', 'LineWidth', 2)
-plot(icdf(tails{index}, pUpperTail), pUpperTail, 'blue' , 'LineWidth', 2)
 
 limits = axis;
 x = linspace(limits(1), limits(2));
-plot(x, normcdf(x, 0, std(returns(:,index))), 'green', 'LineWidth', 2)
 
-fig = gcf;
-fig.Color = [1 1 1];
-hold off
-xlabel('Centered Return')
-ylabel('Probability')
-title (['Semi-Parametric/Piecewise CDF: ' series{index}])
-legend({'Pareto Lower Tail' 'Kernel Smoothed Interior' ...
-        'Pareto Upper Tail' 'Gaussian with Same \sigma'}, 'Location', 'NorthWest')
-    
-% sub 
 
 U = zeros(size(returns));
 
@@ -111,24 +70,6 @@ options     = statset('Display', 'off', 'TolX', 1e-4);
 [rhoT, DoF] = copulafit('t', U, 'Method', 'ApproximateML', 'Options', options);
 rhoG        = copulafit('Gaussian', U);
 
- % linear correlation matrix of daily returns
- disp('linear correlation matrix of daily returns');
- disp(corrcoef(returns));
- 
- % linear correlation matrix of the optimized Gaussian copula
- disp('linear correlation matrix of the optimized Gaussian copula');
- disp(rhoG);
- 
- % linear correlation matrix of the optimized t copula
- disp('linear correlation matrix of the optimized t copula');
- disp(rhoT);
- 
- % scalar degrees of freedom parameter of the optimized t copula
- disp('scalar degrees of freedom parameter of the optimized t copula');
- disp(DoF);
- 
- % Copula Simulaion
- 
  nPoints = 10000;                          % # of simulated observations
 
 s = RandStream.getGlobalStream();
@@ -141,21 +82,7 @@ for j = 1:nIndices
     R(:,j) = icdf(tails{j}, U(:,j));
 end
 
-h = scatterhist(R(:,2), R(:,3),'Color','r','Marker','.','MarkerSize',1);
-fig = gcf;
-fig.Color = [1 1 1];
-y1 = ylim(h(1));
-y3 = ylim(h(3));
-xlim(h(1), [-.1 .1])
-ylim(h(1), [-.1 .1])
-xlim(h(2), [-.1 .1])
-ylim(h(3), [(y3(1) + (-0.1 - y1(1)))  (y3(2) + (0.1 - y1(2)))])
-xlabel('France')
-ylabel('Germany')
-title(['t Copula (\nu = ' num2str(DoF,2) ')'])
-
 % Gaussian Copula
-
 reset(s)
 R = zeros(nPoints, nIndices);             % pre-allocate simulated returns array
 U = copularnd('Gaussian', rhoG, nPoints); % simulate U(0,1) from Gaussian copula
@@ -164,35 +91,8 @@ for j = 1:nIndices
     R(:,j) = icdf(tails{j}, U(:,j));
 end
 
-h = scatterhist(R(:,2), R(:,3),'Color','r','Marker','.','MarkerSize',1);
-fig = gcf;
-fig.Color = [1 1 1];
-y1 = ylim(h(1));
-y3 = ylim(h(3));
-xlim(h(1), [-.1 .1])
-ylim(h(1), [-.1 .1])
-xlim(h(2), [-.1 .1])
-ylim(h(3), [(y3(1) + (-0.1 - y1(1)))  (y3(2) + (0.1 - y1(2)))])
-xlabel('France')
-ylabel('Germany')
-title('Gaussian Copula')
-
-% sub 
 
 reset(s)
-R = mvnrnd(zeros(1,nIndices), cov(returns), nPoints);
-h = scatterhist(R(:,2), R(:,3),'Color','r','Marker','.','MarkerSize',1);
-fig = gcf;
-fig.Color = [1 1 1];
-y1 = ylim(h(1));
-y3 = ylim(h(3));
-xlim(h(1), [-.1 .1])
-ylim(h(1), [-.1 .1])
-xlim(h(2), [-.1 .1])
-ylim(h(3), [(y3(1) + (-0.1 - y1(1)))  (y3(2) + (0.1 - y1(2)))])
-xlabel('France')
-ylabel('Germany')
-title('Gaussian Distribution')
 
 % sub 
 
@@ -203,7 +103,7 @@ r        = mean(yields);             % historical 3M Euribor average
 X        = repmat(100, nIndices, 1); % initial state vector
 strike   = sum(X);                   % initialize an at-the-money basket
 
-nTrials  = 100;                      % # of independent trials
+nTrials  = 10000;                      % # of independent trials
 nPeriods = 63;   % # of simulation periods: 63/252 = 0.25 years = 3 months
 
 % sub 
@@ -220,7 +120,6 @@ GBM2 = gbm(diag(r(ones(1    ,nIndices))), eye(nIndices), 'StartState', X);
 % sub 
 
 f = LongstaffSchwartz(nPeriods, nTrials);
-disp(f);
 
 % sub 
 
@@ -235,7 +134,7 @@ BrownianMotionPutPrice  = f.PutPrice (strike, r);
 reset(s)
 
 z = Example_CopulaRNG(returns * sqrt(252), nPeriods, 'Gaussian');
-f = Example_LongstaffSchwartz(nPeriods, nTrials);
+f = LongstaffSchwartz(nPeriods, nTrials);
 
 simByEuler(GBM2, nPeriods, 'nTrials'  , nTrials, 'DeltaTime', dt, ...
                           'Processes', f.LongstaffSchwartz, 'Z', z);
@@ -248,7 +147,7 @@ GaussianCopulaPutPrice  = f.PutPrice (strike, r);
 reset(s)
 
 z = Example_CopulaRNG(returns * sqrt(252), nPeriods, 't');
-f = Example_LongstaffSchwartz(nPeriods, nTrials);
+f =LongstaffSchwartz(nPeriods, nTrials);
 
 simByEuler(GBM2, nPeriods, 'nTrials'  , nTrials, 'DeltaTime', dt, ...
                           'Processes', f.LongstaffSchwartz, 'Z', z);
